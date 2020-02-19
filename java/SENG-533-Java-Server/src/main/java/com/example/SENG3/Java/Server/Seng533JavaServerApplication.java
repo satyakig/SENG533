291791fa665b9fba22a8561f0658258c30d064b3
Anil Sood <asood11@outlook.com>
2020-02-19 12:22:05 -0700

    Initial commit for the java server

---
+++ java/SENG-533-Java-Server/src/main/java/com/example/SENG3/Java/Server/Seng533JavaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.SENG3.Java.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seng533JavaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seng533JavaServerApplication.class, args);
+	}
+
+}
---
+++ java/SENG-533-Java-Server/src/test/java/com/example/SENG3/Java/Server/Seng533JavaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SENG3.Java.Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seng533JavaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
