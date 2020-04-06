2b168ee2fd4c246c68d316677ca34f487769d68a
Satyaki Ghosh <satyakig901@hotmail.com>
2020-04-06 13:36:33 -0600

    Adding analysis charts and log parsing

--- nodejs/src/app.ts
+++ nodejs/src/app.ts
@@ -13,8 +13,8 @@ initializeMySql();
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(morgan('short'));
 
 app.use('/nosql', noSqlRoute);
